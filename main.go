4f12ca2cc6a9d47ff0959d40118c26500b110fb7
miu <38824651+miu200521358@users.noreply.github.com>
2024-01-08 02:42:39 +0900

    wails.json のバージョン番号を参照

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 
+	"github.com/tidwall/gjson"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -13,20 +14,25 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed wails.json
+var wailsJSON string
+
 func main() {
+	productName := gjson.Get(wailsJSON, "Info.productName").String()
+	productVersion := gjson.Get(wailsJSON, "Info.productVersion").String()
+
 	// Create an instance of the app structure
 	app := app.NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "mmd_rover",
+		Title:  productName + " " + productVersion,
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.StartUp,
+		OnStartup: app.StartUp,
 		Bind: []interface{}{
 			app,
 		},
--- zip_build.bat
+++ zip_build.bat
@@ -1,15 +1,15 @@
 @echo off
-set date00=%date: =0%
-set yyyy=%date00:~0,4%
-set mm=%date00:~5,2%
-set dd=%date00:~8,2%
+setlocal
 
-set time00=%time: =0%
-set hh=%time00:~0,2%
-set mn=%time00:~3,2%
-set ss=%time00:~6,2%
+:: Extract the productVersion from wails.json using PowerShell
+for /f "delims=" %%i in ('powershell -Command "Get-Content wails.json | ConvertFrom-Json | Select -ExpandProperty Info | Select -ExpandProperty productVersion"') do set productVersion=%%i
 
-@echo on
-set filename=mmd_rover_1.00.00_beta_%yyyy%%mm%%dd%_%hh%%mn%%ss%.exe
+:: Display the extracted version
+echo Product Version: %productVersion%
+
+set filename=mmd_rover_%productVersion%.exe
+
 @REM wails build -trimpath -clean -o %filename%
-wails build -v 2 -debug -trimpath -o %filename%
+wails build -trimpath -o %filename%
+
+endlocal
